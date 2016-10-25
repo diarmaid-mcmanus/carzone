065ac46bc4dfe613b146c17a96f6a93571b970ff
Diarmaid <@MadcapOcelot>
2016-10-25 20:54:31 +0100

    Project building using cmake. Sending request & printing response code.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 git submodule --init --recursive
-mkdir build && cd build && cmake .. && make
 
+mkdir build && cd build && cmake .. && make && ./carzone
+
+
+
+
+This will build the project and run it against a sample URL. It prints the
+response code which should be 200.
--- carzone.cpp
+++ carzone.cpp
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <iostream>
 #include <vector>
 #include <string>
+#include <cpr/cpr.h>
 
 int main() {
+    std::cout << "started!" << std::endl;
+
     // get the first page of all vehicle results from carzone
     const char *URL = "carzone.ie/search/ajax-result/cars/page/%d/sort/price-asc/limit/30";
     std::vector<std::string> results;
     int page = 1;
-    //
+    
     // length of results is 0 when pagination finished
     do {
         // local vars
         char url_buffer [1024];
         // format the URL and assign it to a std::string (?)
-//        snprintf(url_buffer, 1023, URL, page);
+        snprintf(url_buffer, 1023, URL, page);
+        std::string ephemeral_url = std::string(url_buffer);
+
+        auto response = cpr::Get(cpr::Url{ephemeral_url});
+
+        std::cout << response.status_code << std::endl;
         
 
     } while (results.size() > 0);
